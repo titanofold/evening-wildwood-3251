385609a98ed532e276bbc3ab41fda4a85ff2eb38
Aaron W. Swenson <aaron.w.swenson@gmail.com>
2014-06-19 06:15:06 -0400

    Add check constraints. Correct typo.

--- deploy/harvestable_types.sql
+++ deploy/harvestable_types.sql
@@ -6,9 +6,9 @@ BEGIN;
 SET client_min_messages = 'warning';
 
 CREATE TABLE ew.harvestable_types (
-       id     CHARACTER VARYING(5) PRIMARY KEY NOT NULL,
+       id     CHARACTER VARYING(5) PRIMARY KEY NOT NULL CHECK (id ~ '^[0-9]{1,5}$'),
        name   TEXT 		   UNIQUE NOT NULL,
-       volume NUMERIC 		   NOT NULL
+       volume NUMERIC 		   NOT NULL CHECK (volume > 0)
 );
 
 COMMIT;
