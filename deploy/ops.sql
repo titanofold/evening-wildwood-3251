96d2be37f62ef2ad0f58fe398782cdbc30a8a08d
Aaron W. Swenson <aaron.w.swenson@gmail.com>
2014-08-17 06:12:01 -0400

    DDL Refine
    Use CHARACTER VARYING instead of just CHARACTER as IDs would be
    right-padded with spaces, making the check fail.
    
    Check that ID is below 2100000000, which is the range for EVE pilot IDs.

--- deploy/ops.sql
+++ deploy/ops.sql
@@ -6,7 +6,7 @@ BEGIN;
 
 CREATE TABLE ew.ops (
        id      SERIAL PRIMARY KEY,
-       foreman CHARACTER VARYING(20) REFERENCES ew.pilots (id),
+       foreman CHARACTER VARYING(10) REFERENCES ew.pilots (id),
        start   TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT current_timestamp,
        finish  TIMESTAMP WITH TIME ZONE
 );
